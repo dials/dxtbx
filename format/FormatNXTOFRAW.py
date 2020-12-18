fbda8cc5d64196807ba02f84f8ab287578d9de4b
davidmcdonagh <davidnmcdonagh@gmail.com>
2020-12-18 10:30:54 +0000

    Removed NXTOFRAWReader class to avoid getters needing to be specific to given .nxs files

--- format/FormatNXTOFRAW.py
+++ format/FormatNXTOFRAW.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 import h5py
 from sys import argv
 from dxtbx.format.FormatHDF5 import FormatHDF5
+from dxtbx import IncorrectFormatError
 
 class FormatNXTOFRAW(FormatHDF5):
 
@@ -15,8 +16,8 @@ class FormatNXTOFRAW(FormatHDF5):
     """
 
     def __init__(self, image_file):
-        self.reader = NXTOFRAWReader(image_file)
-        self.detector = self.reader.get_detector()
+        if not FormatNXTOFRAW.understand(image_file):
+            raise IncorrectFormatError(self, image_file) 
 
     @staticmethod
     def understand(image_file):
@@ -24,46 +25,6 @@ class FormatNXTOFRAW(FormatHDF5):
             return FormatNXTOFRAW.is_nxtofraw_file(image_file)
         except IOError:
             return False
-
-    @staticmethod
-    def is_nxtofraw_file(image_file):
-
-        """
-        Confirms if image_file conforms to NXTOFRAW format
-        by checking if required_fields are present
-
-        """
-
-        required_fields = ["detector_1"]
-
-        def required_fields_present(required_fields, image_file):
-            with h5py.File(image_file, "r") as handle:
-                for i in required_fields:
-                    if not NXTOFRAWReader.field_in_file(i, handle):
-                        return False
-                return True
-
-
-        if not FormatHDF5.understand(image_file):
-            return False
-
-        return required_fields_present(required_fields, image_file)
-
-
-
-class NXTOFRAWReader:
-
-    """
-    Class to hold NXTOFRAW files in memory
-    and convert NXTOFRAW values to formats useful for FormatNXTOFRAW
-    """
-    
-    def __init__(self, nxs_filename):
-        self.nxs_filename = nxs_filename
-        self.nxs_file = self.open_nxs_file(nxs_filename)
-
-    def open_nxs_file(self, nxs_file):
-        return h5py.File(nxs_file, "r")
 
     @staticmethod
     def field_in_file(field, nxs_file):
@@ -83,11 +44,30 @@ class NXTOFRAWReader:
                 return True
         return False
 
-    def get_detector(self):
-        return self.nxs_file['/raw_data_1/detector_1']
+    @staticmethod
+    def is_nxtofraw_file(image_file):
 
-    def get_name(self):
-        return nxs_file['/raw_data_1/name'][0].decode()
+        """
+        Confirms if image_file conforms to NXTOFRAW format
+        by checking if required_fields are present
+
+        """
+
+        required_fields = ["detector_1"]
+
+        def required_fields_present(required_fields, image_file):
+            with h5py.File(image_file, "r") as handle:
+                for i in required_fields:
+                    if not FormatNXTOFRAW.field_in_file(i, handle):
+                        return False
+                return True
+
+
+        if not FormatHDF5.understand(image_file):
+            return False
+
+        return required_fields_present(required_fields, image_file)
+
 
 
 if __name__== "__main__":
